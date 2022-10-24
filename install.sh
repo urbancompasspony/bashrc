f2d9d2ffba04c64becc09b4ca0481e401eba774c
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-10-24 00:21:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+[ "$EUID" -ne 0 ] || {
+  echo "Não execute esse script como Root!"
+  exit
+  }
+
 curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc > $HOME/.bashrc
