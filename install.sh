efb173695961f0f588df7491ca3a034ead9a7852
Zóio - The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2024-02-23 13:52:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-[ "$EUID" -ne 0 ] || {
-  echo "Não execute esse script como Root, ainda!"
-  exit
-  }
+curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc | tee $HOME/.bashrc
 
-curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc | sudo tee $HOME/.bashrc
-
-sudo rm /root/.bashrc
+#sudo rm /root/.bashrc
 curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc | sudo tee /root/.bashrc
 
 echo "Novo bashrc instalado para os usuários $USER e ROOT!"
