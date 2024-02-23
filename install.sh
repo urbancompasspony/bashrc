59c05a6137dadbb5828ac8710e27797f41c6a64e
Zóio - The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2024-02-22 23:37:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@
 curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc | sudo tee $HOME/.bashrc
 
 sudo rm /root/.bashrc
-curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/root/.bashrc | sudo tee /root/.bashrc
+curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc | sudo tee /root/.bashrc
 
 echo "Novo bashrc instalado para os usuários $USER e ROOT!"
+sleep 3
