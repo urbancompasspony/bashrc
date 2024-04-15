89e114d4dcbbe6c2320e9fd06b7abc903eadd7fe
Zóio - The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2024-04-15 00:12:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc | tee $HOME/.bashrc
+clear
+echo  -e "\033[39;5m==========================================\033[0m"
+echo  -e "\033[39;5mDigite a senha de administrador por favor!\033[0m"
+echo  -e "\033[39;5m==========================================\033[0m"
 curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc | sudo tee /root/.bashrc
 curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/.bashrc | sudo tee /etc/bash.bashrc
 
